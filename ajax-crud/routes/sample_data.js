d8819a5d4e864eee3d0e5b98ade8fdf97bf3d7b3
sebestral-solio <155797750+sebestral-solio@users.noreply.github.com>
2024-03-02 17:28:51 +0530

    Update sample_data.js

--- ajax-crud/routes/sample_data.js
+++ ajax-crud/routes/sample_data.js
@@ -27,12 +27,24 @@ axios.get('http://localhost:3000/action')
   .catch(error => {
     console.error('Error:', error);
   });
+
+
 router.all("/api/*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   return next();
 });
+
+
+app.all("/api/*", function(req, res, next) {
+  if (req.method.toLowerCase() !== "options") {
+    return next();
+  }
+  return res.send(204);
+});
+
+
 router.get("/8", function(req, res, next){
 	if (req.session.user) {
 	res.render('sample_data', {title : 'Class I To VIII'});
