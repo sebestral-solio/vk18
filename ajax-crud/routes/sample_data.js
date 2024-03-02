55c3ba90dd2d774bbc625285ab8869ef8bde3bb7
sebestral-solio <155797750+sebestral-solio@users.noreply.github.com>
2024-03-02 17:39:58 +0530

    Update sample_data.js

--- ajax-crud/routes/sample_data.js
+++ ajax-crud/routes/sample_data.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 router.use(cors({
-  origin: 'http://localhost:3000', // Replace with your allowed origin
+  origin: 'http://localhost:3000/action', // Replace with your allowed origin
   methods: 'GET,POST', // Specify allowed methods
   credentials: true // Allow sending cookies
 }));
